2352b332fc39ef3f1de04a7f53f3b0577bbc2df6
YudiHardiyanta <yudihardiyanta@gmail.com>
2025-05-27 14:19:48 +0800

    approve dan reject oleh pengawas

--- controller/dataController.js
+++ controller/dataController.js
@@ -5,6 +5,46 @@ dotenv.config();  // Memuat variabel lingkungan dari file .env
 
 const prisma = new PrismaClient();
 
+export async function updateStatusById(req, res) {
+    try {
+        const id_sampel = req.body.id
+        const status = req.body.status
+        const sampel = await prisma.sampelKegiatan.findUnique({
+            where: {
+                id: parseInt(id_sampel)
+            },
+            include: {
+                MDesa: true
+            }
+        })
+        if (!sampel) {
+            console.log('sampel tidak ketemu')
+            return res.status(401).json({ code: 401, message: 'Anda tidak memiliki akses untuk pendataan ini' });
+        }
+        if (sampel.pengawas_email != req.user.username) {
+            return res.status(401).json({ code: 401, message: 'Anda tidak memiliki akses untuk pendataan ini' });
+        }
+        const updated_sampel = await prisma.sampelKegiatan.update({
+            where: {
+                id: parseInt(id_sampel)
+            },
+            data: {
+                status: status
+            }
+        });
+        return res.status(200).json({
+            code: 200,
+            message : `Data ${sampel.MDesa.nama} sudah ${updated_sampel.status}`
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            code: 500, message: error.message
+        })
+    }
+
+}
+
 export async function getDataById(req, res) {
     try {
         const id_sampel = req.query.id
@@ -12,30 +52,30 @@ export async function getDataById(req, res) {
         // get data sampel
         const sampel = await prisma.sampelKegiatan.findUnique({
             where: {
-                id : parseInt(id_sampel)
+                id: parseInt(id_sampel)
             },
-            include : {
-                MProv : true,
-                MKab : true,
-                MKec : true,
-                MDesa : true,
-                answerKegiatan : true
+            include: {
+                MProv: true,
+                MKab: true,
+                MKec: true,
+                MDesa: true,
+                answerKegiatan: true
             }
-            
+
         })
         const id_kegiatan = sampel.kegiatan_id;
-        
+
         // cek user role
         const roleId = req.user.role.find(r => r.kegiatan_id == id_kegiatan);
-        console.log(req.user)
+        console.log(roleId)
         if (!roleId) {
             res.status(401).json({ code: 401, message: 'Anda tidak memiliki akses untuk pendataan ini' });
         }
-        
+
         return res.status(200).json({
             code: 200,
             data: sampel,
-            role : roleId.role
+            role: roleId.role
         })
 
     } catch (error) {
@@ -43,8 +83,4 @@ export async function getDataById(req, res) {
             code: 500, message: error.message
         })
     }
-    
-    
-
-    
 }
--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getDataById } from '../controller/dataController.js';
+import { getDataById,updateStatusById} from '../controller/dataController.js';
 import verifyToken from '../middleware/authMiddleware.js';
 
 const dataRoutes = express.Router();
 
 dataRoutes.get('/',verifyToken,getDataById)
+dataRoutes.post('/verify',verifyToken,updateStatusById)
 
 export default dataRoutes
