5237b6924164ba0fa2b484bfc3348f97a80a2fae
YudiHardiyanta <yudihardiyanta@gmail.com>
2025-05-30 22:50:21 +0800

    menambahkan keterangan total aggregasi

--- controller/sampelController.js
+++ controller/sampelController.js
@@ -20,19 +20,23 @@ export async function getSampel(req, res) {
         if (roleId.role == 'pengawas') {
             whereClause.pengawas_email = roleId.user_email
         }
+
+        if(req.query.status!='all'){
+            whereClause.status=req.query.status
+        }
         const sampel = await prisma.sampelKegiatan.findMany({
             where: whereClause,
-            include : {
-                MProv : true,
-                MKab : true,
-                MKec : true,
-                MDesa : true
+            include: {
+                MProv: true,
+                MKab: true,
+                MKec: true,
+                MDesa: true
             }
         })
         return res.status(200).json({
             code: 200,
             data: sampel,
-            role : roleId.role
+            role: roleId.role
         })
     } catch (error) {
         return res.status(500).json({
@@ -40,4 +44,35 @@ export async function getSampel(req, res) {
         })
     }
 
+}
+
+export async function aggSampel(req, res) {
+    try {
+        const roleId = req.user.role.find(r => r.kegiatan_id == req.query.id);
+        if (!roleId) {
+            res.status(401).json({ code: 401, message: 'Anda tidak memiliki akses untuk pendataan ini' });
+        }
+        const whereClause = {}
+        if (roleId.role == 'pencacah') {
+            whereClause.pencacah_email = roleId.user_email
+        }
+        if (roleId.role == 'pengawas') {
+            whereClause.pengawas_email = roleId.user_email
+        }
+        const agg = await prisma.sampelKegiatan.groupBy({
+            where: whereClause,
+            by: ['status'],
+            _count: {
+                _all: true,
+            },
+        })
+        return res.status(200).json({
+            code: 200,
+            data: agg,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            code: 500, message: error.message
+        })
+    }
 }
--- routes/sampelRoutes.js
+++ routes/sampelRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getSampel } from '../controller/sampelController.js';
+import { getSampel,aggSampel } from '../controller/sampelController.js';
 import verifyToken from '../middleware/authMiddleware.js';
 
 const sampelRoutes = express.Router();
 
 sampelRoutes.get('/',verifyToken,getSampel)
+sampelRoutes.get('/agg',verifyToken,aggSampel)
 
 export default sampelRoutes
