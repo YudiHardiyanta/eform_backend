aa78a63cb899037f1c726f4087cebf4f10239b3b
YudiHardiyanta <yudihardiyanta@gmail.com>
2025-07-14 11:38:24 +0800

    fitur reset password

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { compare } from "bcrypt";
+import { compare,hash } from "bcrypt";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
@@ -37,6 +37,45 @@ export async function login(req, res) {
         return res.status(200).json({ token });
 
     } catch (error) {
+        return res.status(500).json({
+            code: 500, message: error.message
+        })
+    }
+}
+
+export async function reset_password(req, res) {
+    const { password_lama, password_baru } = req.body;
+    console.log(password_lama)
+    const user_login = req.user
+    console.log(req.user)
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: user_login.username
+            },
+            include: {
+                userRoles: true,
+            },
+        })
+        const isMatch = await compare(password_lama, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ code: 401, message: 'Password lama salah' });
+        }
+
+        const hashedPassword = await hash(password_baru, 10);
+
+        const updated_user = await prisma.user.update({
+            where: {
+                email: user_login.username
+            },
+            data: {
+                password: hashedPassword,
+            }
+        });
+        return res.status(200).json({ code:200,message : 'password sudah berhasil diganti' });
+
+    } catch (error) {
+        console.log(error)
         return res.status(500).json({
             code: 500, message: error.message
         })
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { login } from '../controller/userController.js';
+import { login,reset_password } from '../controller/userController.js';
+import verifyToken from '../middleware/authMiddleware.js';
 
 const userRoutes = express.Router();
 
 userRoutes.post('/login',login)
+userRoutes.post('/reset_password',verifyToken,reset_password)
 
 export default userRoutes
